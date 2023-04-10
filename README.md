ad621057253f1060ef1fba9aed3374c799481417
Fireheart182 <alyssweissglass@gmail.com>
2023-04-10 13:10:28 -0700

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vague-bot
 Our Discord bot
+
+
+---
+## Contributing
+
+### Set up your local environment
+**Install the Discord SDK**
+
+```bash
+python3 -m pip install -U discord.py
+```
---
+++ src/hello_bot.py
@@ -0,0 +1,21 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class VaguebotClient(discord.Client):
+    async def on_ready(self):
+        print(f"Logged on as {self.user}!")
+
+    async def on_message(self, message):
+        print(f"Message from {message.author}: {message.content}")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = VaguebotClient(intents=intents)
+client.run(os.getenv("DISCORD_TOKEN"))
